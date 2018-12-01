8d7a371ce168956695fb4982afc87aee7ed9bd59
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-01 21:41:20 +0000

    working with JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,38 +1,67 @@
 'use strict';
 
+var appInfo = {
+    title: 'React.js',
+    subtitle: 'the best framework to create apps.',
+    apps: ['App 1', 'App 2', 'App 3']
+};
+
 var template = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        'This is JSX from app.js!'
+        appInfo.title
     ),
     React.createElement(
         'p',
         null,
-        'This is some info.'
+        appInfo.subtitle
     ),
     React.createElement(
         'ul',
         null,
-        React.createElement(
-            'li',
-            null,
-            'Item1'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item2'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item3'
-        )
+        appInfo.apps.map(function (item) {
+            return React.createElement(
+                'li',
+                { key: item },
+                item
+            );
+        })
     )
 );
+
+// EXERCISE:
+
+var user = {
+    name: 'Mike',
+    age: 25,
+    location: 'Uk'
+};
+
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name.toUpperCase() + '!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
+);
+
 var appRoute = document.getElementById('appRoute');
 
 ReactDOM.render(template, appRoute);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,16 +1,38 @@
 
 
+const appInfo = {
+    title: 'React.js',
+    subtitle: 'the best framework to create apps.',
+    apps: ['App 1', 'App 2', 'App 3']
+}
+
 const template = (
     <div>
-        <h1>This is JSX from app.js!</h1>
-        <p>This is some info.</p>
+        <h1>{appInfo.title}</h1>
+        <p>{appInfo.subtitle}</p>
         <ul>
-            <li>Item1</li>
-            <li>Item2</li>
-            <li>Item3</li>
+            {appInfo.apps.map(item => <li key={item}>{item}</li>)}
         </ul>
     </div>
 );
+
+// EXERCISE:
+
+const user = {
+    name: 'Mike',
+    age: 25,
+    location: 'Uk'
+};
+
+const template2 = (
+    <div>
+        <h1>{user.name.toUpperCase() + '!'}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
 const appRoute = document.getElementById('appRoute');
 
 ReactDOM.render(template, appRoute);
+
