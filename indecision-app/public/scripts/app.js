5ca6465baa23db14bf527ba7049158ea37a581fa
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-01 21:13:27 +0000

    formating jsx

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,9 +1,37 @@
 'use strict';
 
 var template = React.createElement(
-  'h1',
-  null,
-  'This is JSX from app.js!'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'This is JSX from app.js!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info.'
+    ),
+    React.createElement(
+        'ul',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item2'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item3'
+        )
+    )
 );
 var appRoute = document.getElementById('appRoute');
 
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,16 @@
 
 
-const template = <h1>This is JSX from app.js!</h1>
+const template = (
+    <div>
+        <h1>This is JSX from app.js!</h1>
+        <p>This is some info.</p>
+        <ul>
+            <li>Item1</li>
+            <li>Item2</li>
+            <li>Item3</li>
+        </ul>
+    </div>
+);
 const appRoute = document.getElementById('appRoute');
 
 ReactDOM.render(template, appRoute);
