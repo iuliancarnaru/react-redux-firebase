6deae5fb5303949f2e8be1a54c6c43c0afbd6023
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-02 20:18:44 +0000

    toggle visibility exercise

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,42 +1,14 @@
 'use strict';
 
-/* 
-
-React dom elements
-React dom events
-e = event
-preventDefault - prevent full page refresh
-
-INFO: JSX support stings, numbers and arrays by default. It doesn't support objects and ignore booleans, null or undefined by not rendering them. We can render JSX inside JSX. Adding key to elements hepls JSX to optimize renderind.
-
-*/
+// VISIBILITY TOGGLE EXERCISE
 
 var app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer',
-    options: []
+    title: 'Visibility Toggle',
+    visibility: false
 };
 
-var onFormSubmit = function onFormSubmit(e) {
-    e.preventDefault();
-
-    // get the value that user type
-    var option = e.target.elements.option.value;
-
-    //check if there is any option
-    if (option) {
-        app.options.push(option);
-
-        //clear the input
-        e.target.elements.option.value = '';
-
-        //render the new data
-        renderApp();
-    }
-};
-
-var removeOptions = function removeOptions() {
-    app.options = [];
+var toggleVisibility = function toggleVisibility() {
+    app.visibility = !app.visibility;
     renderApp();
 };
 
@@ -49,54 +21,15 @@ var renderApp = function renderApp() {
             null,
             app.title
         ),
-        app.subtitle && React.createElement(
-            'p',
-            null,
-            app.subtitle
-        ),
         React.createElement(
-            'p',
-            null,
-            app.options.length > 0 ? 'Here are your options:' : 'No options.'
+            'button',
+            { onClick: toggleVisibility },
+            app.visibility ? 'Hide details' : 'Show details'
         ),
-        React.createElement(
-            'ul',
+        app.visibility && React.createElement(
+            'h3',
             null,
-            app.options.map(function (option, index) {
-                return React.createElement(
-                    'li',
-                    { key: index + 1 },
-                    option
-                );
-            })
-        ),
-        React.createElement(
-            'p',
-            null,
-            'You have ',
-            app.options.length,
-            ' ',
-            app.options.length === 1 ? 'option' : 'options',
-            ' available.'
-        ),
-        React.createElement(
-            'p',
-            null,
-            app.options.length > 0 && React.createElement(
-                'button',
-                { onClick: removeOptions },
-                'Remove all options'
-            )
-        ),
-        React.createElement(
-            'form',
-            { onSubmit: onFormSubmit },
-            React.createElement('input', { type: 'text', name: 'option' }),
-            React.createElement(
-                'button',
-                null,
-                'Add option'
-            )
+            'Now you see me!'
         )
     );
 
