912d74275ec52ad6b4212637fbb14bd9bb6cf524
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-04 18:54:38 +0000

    adding normalize.css

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import IndecisionApp from './components/IndecisionApp';
 
+import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 
