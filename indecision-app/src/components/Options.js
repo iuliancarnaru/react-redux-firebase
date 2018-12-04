ece80f353fb24df7e78538f7aee4e83bf17bd5bc
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-04 22:16:45 +0000

    styling the app

--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -4,12 +4,19 @@ import Option from './Option';
 
 const Options = (props) => (
     <div>
-        <button onClick={props.handleDeleteOptions}>Remove All</button>
-        {props.options.length === 0 && <p>Please add an option to get started</p>}
+        <div className="widget-header">
+            <h3>Your options</h3>
+            <button 
+                onClick={props.handleDeleteOptions}
+                className="button button--link"
+            >Remove All</button>
+        </div>
+        {props.options.length === 0 && <p className="widget__message">Please add an option to get started</p>}
         {props.options.map((option, index) => (
             <Option 
                 key={index+1} 
                 optionText={option} 
+                count={index+1}
                 handleDeleteOption={props.handleDeleteOption}
             />
         ))}
