bcf03910a002f032fb1b3e5d4b11cf66db40d3eb
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-07 18:27:04 +0000

    setting up the expensify app boilerplate

---
+++ boilerplate/src/app.js
@@ -0,0 +1,12 @@
+
+
+import React from 'react';
+import ReactDOM from "react-dom";
+
+
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+
+ReactDOM.render(<Example /> , document.getElementById('app'));
+
