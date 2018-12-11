e02413c7e75705baa88baeff0188e58989c83d87
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-11 20:29:14 +0000

    create edit page with fields persistance

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -5,14 +5,17 @@ import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 
 class ExpenseForm extends Component {
+    constructor(props) {
+        super(props);
 
-    state = {
-        description: '',
-        note: '',
-        amount: '',
-        createdAt: moment(),
-        calendarFocused: false,
-        error:''
+        this.state = {
+            description: props.expense ? props.expense.description : '',
+            note: props.expense ? props.expense.note : '',
+            amount: props.expense ? (props.expense.amount / 100).toString() : '',
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+            calendarFocused: false,
+            error:''
+        }
     }
 
     onDescriptionChange = (e) => {
