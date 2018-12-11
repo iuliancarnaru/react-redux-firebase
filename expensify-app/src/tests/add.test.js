a38e122725ce922922e3de95d26b52492b0bb4b8
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-11 21:40:04 +0000

    learning jest and creating first dummy tests

---
+++ expensify-app/src/tests/add.test.js
@@ -0,0 +1,12 @@
+const add = (a,b) => a+b;
+const generateGreeting = (name) => `Hello ${name}!`
+
+test('should add two numbers', () => {
+    const result = add(3,4);
+    expect(result).toBe(7);
+})
+
+test('should return a string', () => {
+    const result = generateGreeting('Iulian');
+    expect(result).toBe(`Hello Iulian!`);
+})
