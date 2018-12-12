0664f2594f91968133bdb1bef55c984b2d5a5276
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-12 19:16:38 +0000

    update

--- expensify-app/src/tests/add.test.js
+++ expensify-app/src/tests/add.test.js
@@ -1,12 +1,17 @@
 const add = (a,b) => a+b;
-const generateGreeting = (name) => `Hello ${name}!`
+const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`
 
 test('should add two numbers', () => {
     const result = add(3,4);
     expect(result).toBe(7);
 })
 
-test('should return a string', () => {
+test('should return a greeting from name', () => {
     const result = generateGreeting('Iulian');
     expect(result).toBe(`Hello Iulian!`);
-})
+});
+
+test('should return a greeting from no name', () => {
+    const result = generateGreeting();
+    expect(result).toBe(`Hello Anonymous!`);
+});
