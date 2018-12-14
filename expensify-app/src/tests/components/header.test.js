bb8111181f7d03775a60143cda960bf08864a6a3
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-14 21:11:01 +0000

    setting up airbnb Enzyme with Jest

--- expensify-app/src/tests/components/header.test.js
+++ expensify-app/src/tests/components/header.test.js
@@ -6,7 +6,4 @@ test('should render Header correctly', () => {
     const renderer = new ReactShallowRenderer();
     renderer.render(<Header />);
     expect(renderer.getRenderOutput()).toMatchSnapshot();
-
-
-    console.log(renderer.getRenderOutput());
 });
