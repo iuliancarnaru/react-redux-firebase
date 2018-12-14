c51e11b76a485effd1effd250bf1c1f123f44251
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-14 21:57:26 +0000

    test ExpenseList component

--- expensify-app/src/components/ExpenseList.js
+++ expensify-app/src/components/ExpenseList.js
@@ -4,12 +4,18 @@ import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
     <div>
         <h1>Expense List</h1>
-        {props.expenses.map((expense) => {
-            return <ExpenseListItem key={expense.id} {...expense}/>
-        })}
+        {
+            props.expenses.length === 0 ? (
+                <p>No expenses found</p>
+            ) : (
+                props.expenses.map((expense) => {
+                    return <ExpenseListItem key={expense.id} {...expense}/>
+                })
+            )
+        }
     </div>
 );
 
