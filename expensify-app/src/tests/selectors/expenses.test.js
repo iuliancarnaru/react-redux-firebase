39e9d677a26c52d6b96093451b79f8407bc9c059
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-14 20:32:02 +0000

    testing expenses reducer

--- expensify-app/src/tests/selectors/expenses.test.js
+++ expensify-app/src/tests/selectors/expenses.test.js
@@ -1,29 +1,7 @@
 import selectExpenses from '../../selectors/expenses';
 import moment from 'moment';
+import expenses from '../fixtures(test data)/expenses';
 
-const expenses = [
-    {
-        id: 1,
-        description: 'Gum',
-        note: '',
-        amount: 195,
-        createdAt: 0
-    },
-    {
-        id: 2,
-        description: 'Rent',
-        note: '',
-        amount: 19500,
-        createdAt: moment(0).subtract(4, 'days').valueOf()
-    },
-    {
-        id: 3,
-        description: 'Credit Card',
-        note: '',
-        amount: 4500,
-        createdAt: moment(0).add(4, 'days').valueOf()
-    },
-]
 test('should filter by text value', () => {
     const filters = {
         text: 'e',
