541afb4e71d71e94ca7d34b049c18a44e5ea45fe
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-15 19:49:12 +0000

    learning about Enzyme global lifecycle methods

---
+++ expensify-app/src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import expenses from '../fixtures(test data)/expenses';
+import { AddExpensePage } from '../../components/AddExpensePage';
+
+let onSubmit, history, wrapper;
+
+beforeEach(() => {
+    onSubmit = jest.fn();
+    history = { push: jest.fn() }
+    wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history}/>);
+});
+
+test('should render AddExpensePage correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle onSubmit', () => {
+    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
+    expect(history.push).toHaveBeenLastCalledWith('/');
+    expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
+});
