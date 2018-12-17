b0ce9054f12af687c92f53ecc4b6c3d38702c939
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-17 21:03:51 +0000

    adding summary component

--- expensify-app/src/tests/fixtures(test data)/expenses.js
+++ expensify-app/src/tests/fixtures(test data)/expenses.js
@@ -12,7 +12,7 @@ const expenses = [
         id: 2,
         description: 'Rent',
         note: '',
-        amount: 19500,
+        amount: 109500,
         createdAt: moment(0).subtract(4, 'days').valueOf()
     },
     {
